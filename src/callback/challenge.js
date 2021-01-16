3aa2cb5cb2d5bf4289b519697343d1a6f4b0d9b4
Nahuel Farias <nahuelfarias812@gmail.com>
2021-01-16 15:15:01 -0300

    Aprendiendo callback

---
+++ src/callback/challenge.js
@@ -0,0 +1,17 @@
+let XMLHtppRequest = require("xmlhttprequest").XMLHttpRequest;
+
+function fetchData(url_api, callback) {
+  let xhttp = new XMLHtppRequest();
+  xhttp.open("GET", url_api, true);
+  xhttp.onreadystatechange = function (event) {
+    if (xhttp.readyState === 4) {
+      if (xhttp.status === 200) {
+        callback(null, JSON.parse(xhttp.responseText)); // Aca parseamos de JSON a TEXTO
+      } else {
+        const error = new Error("Error" + url_api);
+        return callback(error, null);
+      }
+    }
+  };
+  xhttp.send();
+}
